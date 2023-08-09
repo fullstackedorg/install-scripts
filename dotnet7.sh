73b68fb597324d04b0974e3e3a2fddd78ffb0117
CP Lepage <32472542+cplepage@users.noreply.github.com>
2023-08-09 00:28:36 -0400

    Update dotnet7.sh

--- dotnet7.sh
+++ dotnet7.sh
@@ -1,6 +1,7 @@
 wget https://download.visualstudio.microsoft.com/download/pr/2cd3ad74-b58e-472b-bb0b-318b951c0ab4/dee685848e73c9fa85c93112cbac89ac/dotnet-sdk-7.0.306-linux-musl-x64.tar.gz
-mkdir -p $HOME/dotnet && tar zxf dotnet-sdk-7.0.306-linux-musl-x64.tar.gz -C $HOME/dotnet
-echo "export DOTNET_ROOT=\$HOME/dotnet" >> $HOME/.profile
-echo "export PATH=\$PATH:\$HOME/dotnet" >> $HOME/.profile
+mkdir -p $HOME/.dotnet-sdk
+tar zxf dotnet-sdk-7.0.306-linux-musl-x64.tar.gz -C $HOME/.dotnet-sdk
+echo "export DOTNET_ROOT=\$HOME/.dotnet-sdk" >> $HOME/.profile
+echo "export PATH=\$PATH:\$HOME/.dotnet-sdk" >> $HOME/.profile
 rm -rf dotnet-sdk-7.0.306-linux-musl-x64.tar.gz
 source $HOME/.profile
